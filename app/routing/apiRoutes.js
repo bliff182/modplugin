0945a147af498e65e45922a5fcbbbdc6935a843f
bliff182 <michael.bither@gmail.com>
2019-11-25 16:14:20 -0500

    html added, routing established

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friendData = require('../data/friends');
+
+module.exports = function(app) {
+
+    app.get('/api/friends', function(req, res) {
+        res.json(friendData);
+    });
+
+    // POST - will be used to handle incoming survey results and compatibility logic
+    // app.post('/api/friends', function(req, res) {
+
+    // });
+
+};
