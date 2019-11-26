b682b58b905f3968fe5c3d2d524248e5740feba5
bliff182 <michael.bither@gmail.com>
2019-11-26 11:12:04 -0500

    post working, match showing after form submission

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,31 @@ module.exports = function(app) {
 
     // POST - will be used to handle incoming survey results and compatibility logic
     app.post('/api/friends', function(req, res) {
+        var newFriend = req.body;
+        var newScores = newFriend.scores;
+        var differenceArray = []; // to hold each friend's score difference for comparison purposes
+        var matchIndex = 0; // for use as an index when we compare all scores, will hold index of best match
+
+        // parsing through friendArray
+        for (var i = 0; i < friendData.length; i++) {
+            var friendScores = friendData[i].scores;
+            var scoreDifference = 0; // resets the score difference for each friend it parses
+
+            // parsing through friend scores specifically
+            for (var j = 0; j < newScores.length; j++) {
+                scoreDifference += Math.abs(friendScores[j] - newScores[j]); // difference between user scores and already stored friend scores
+            }
+            differenceArray.push(scoreDifference);
+        }
+        // console.log(differenceArray);
+        for (var k = 0; k < differenceArray.length; k++) {
+            if (differenceArray[k] <= differenceArray[matchIndex]) {
+                matchIndex = k;
+            }
+        }
+        // console.log(friendData[matchIndex]);
+        res.json(friendData[matchIndex]);
         friendData.push(req.body);
-        // console.log(req.body);
     });
 
 };
